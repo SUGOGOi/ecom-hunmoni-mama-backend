ad5c4f87fd0b1978af664d66415f56906c16d667
Sumsum Gogoi <104547345+SUGOGOi@users.noreply.github.com>
2024-03-19 12:19:30 +0530

    db

--- src/config/DBconfig.ts
+++ src/config/DBconfig.ts
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const { connection } = await mongoose.connect(`${process.env.MONGO_C0}`);
+    const { connection } = await mongoose.connect(
+      `mongodb+srv://sumsum:sumsumgogoi@cluster0.5sqlsyx.mongodb.net/ecom_hunmoni?retryWrites=true&w=majority&appName=Cluster0`
+    );
     // const { connection } = await mongoose.connect(`${process.env.MONGO_LOCAL}`);
     console.log(`Mongodb connected ${connection.host}`);
   } catch (error) {
